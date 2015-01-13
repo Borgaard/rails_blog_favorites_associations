dc8797d0cf907b418c39eec19c366d7b86d5aade
nicoleborgaard@gmail.com <nicoleborgaard@gmail.com>
2015-01-13 11:08:36 -0800

    Add user table association with article table with foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :articles, dependent: :destroy #user.articles method now available to use
+
+  
 end
